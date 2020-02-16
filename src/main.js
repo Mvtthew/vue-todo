b9a9d3d7efe6f272ec2948709996a95399fcfa61
Mateusz Ozog <mateusz2345678@gmail.com>
2020-02-16 13:39:04 +0100

    Working Todo app!

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
 
-Vue.config.productionTip = false
+import Vue from 'vue';
+import App from './App.vue';
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+	render: h => h(App),
+}).$mount('#app');
